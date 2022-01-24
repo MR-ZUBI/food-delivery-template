c98fe9ec09af645a099f2bfb5b9b9c6109a80185
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-01-24 18:31:31 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var menuHamburger = document.getElementById("hamburger");
+var menuCross = document.getElementById("cross");
+menuHamburger.addEventListener("click", () => {
+    window.alert("Hamburger clicked");
+});
+menuCross.addEventListener("click", () => {
+    window.alert("Cross clicked");
+});
