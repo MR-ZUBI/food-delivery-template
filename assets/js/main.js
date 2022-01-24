4e11c8e34cf13b219be3823d721a6d775157522c
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-01-24 23:20:30 +0600

    added menu for mobile device

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
-var menuHamburger = document.getElementById("hamburger");
-var menuCross = document.getElementById("cross");
+const nav = document.getElementById("nav");
+const menuMain = document.getElementById("main-menu");
+const menuHamburger = document.getElementById("hamburger");
+const menuCross = document.getElementById("cross");
+
 menuHamburger.addEventListener("click", () => {
-    window.alert("Hamburger clicked");
+    menuMain.style.height = "210px";
+    menuMain.style.display = "block";
+    menuCross.style.display = "block";
+    menuHamburger.style.display = "none";
+    nav.style.borderColor = "gray";
 });
+
 menuCross.addEventListener("click", () => {
-    window.alert("Cross clicked");
+    menuMain.style.height = "0px";
+    menuMain.style.display = "none";
+    menuCross.style.display = "none";
+    menuHamburger.style.display = "block";
+    nav.style.borderColor = "transparent";
 });
